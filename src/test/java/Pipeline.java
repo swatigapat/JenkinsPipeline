f1dbddc3e3bd173cdc31cd512f280f1778b47c80
Your Name <swatigapat04@gmail.com>
2022-10-24 10:42:13 +0530

    Jenkins Pipeline

--- src/test/java/Pipeline.java
+++ src/test/java/Pipeline.java
@@ -1,9 +1,18 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
 
 public class Pipeline {
 @Test
         void myFirstPipeline(){
-    WebDriverbDriver driver=new ChromeDriver();
+    WebDriverManager.chromedriver().setup();
+    ChromeOptions options=new ChromeOptions();
+    options.addArguments("--incognito");
+    options.addArguments("--headless");
+    WebDriver driver=new ChromeDriver(options);
 
     // Launch Website
     driver.navigate().to("http://www.javatpoint.com/");
